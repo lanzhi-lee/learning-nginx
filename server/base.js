657975c1b6f7d3eda2e05a806d743c9ed3d701dc
Lanzhi <lanzhi_lee@163.com>
2021-11-19 19:32:32 +0800

    docs: 1.reverse-proxy

--- server/base.js
+++ server/base.js
@@ -5,8 +5,18 @@ const App = new Koa()
 const Router = new KoaRouter()
 
 module.exports = (port) => {
+  // log request URL:
+  App.use(async (ctx, next) => {
+    console.log(`${port} Process ${ctx.request.method} ${ctx.request.url}...`)
+    await next()
+  })
+
   Router.get('/', async (ctx) => {
     ctx.response.body = `<h1>Index ${port}</h1>`
+  })
+  // 1.reverse-proxy
+  Router.get('/99981', async (ctx) => {
+    ctx.response.body = `<h1>Index 99981</h1>`
   })
 
   App.use(Router.routes())
